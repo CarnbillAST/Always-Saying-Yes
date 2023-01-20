8377308289b6a97b1eabd2089e90c29db5a33ee5
CarnbillAST <122953219+CarnbillAST@users.noreply.github.com>
2023-01-20 09:28:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
 
     // typing animation script
     var typed = new Typed(".typing",{
-        strings: ["Your Skill","Your Hobby","Your Passion","Your Proficiency"],
+        strings: ["Biker","Digital Marketing Expert","Music Lover","Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
@@ -40,7 +40,7 @@ $(document).ready(function () {
         
     });
     var typed = new Typed(".typing-2",{
-        strings: ["Your Skill","Your Hobby","Your Passion","Your Proficiency"],
+        strings: ["Biker","Digital Marketing Expert","Music Lover","Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
